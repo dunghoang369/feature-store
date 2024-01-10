984b620f147bb963e47c5a2a37d476c9fed5ce2d
dunghoang369 <dung.hoang300699@gmail.com>
2024-01-10 11:39:12 +0700

    release code

---
+++ datalake/deltalake/write_deltalake_format.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+from glob import glob
+
+import pandas as pd
+from helpers import load_cfg
+
+from deltalake.writer import write_deltalake
+
+CFG_PATH = "../../utils/config.yaml"
+
+if __name__ == "__main__":
+    # Load config.yaml
+    cfg = load_cfg(CFG_PATH)
+
+    # Get csv files
+    csv_files = glob("../data/diabetes/*.csv")
+
+    # Write data into deltalake format
+    for csv_file in csv_files:
+        file_name = csv_file.split("/")[-1].split(".")[0]
+        df = pd.read_csv(csv_file)
+        if os.path.exists(
+            os.path.join(cfg["datastore"]["diabetes_deltalake_path"], file_name)
+        ):
+            shutil.rmtree(
+                os.path.join(cfg["datastore"]["diabetes_deltalake_path"], file_name)
+            )
+        write_deltalake(
+            os.path.join(cfg["datastore"]["diabetes_deltalake_path"], file_name), df
+        )
+        print(f"Generated the file {file_name} successfully!")
+
+    df = pd.read_csv("../data/diabetes/diabetes.csv")
+    write_deltalake("../data/diabetes-deltalake/diabetes", df)
---
+++ datalake/deltalake/write_new_data.py
@@ -0,0 +1,53 @@
+import random
+
+import numpy as np
+import pandas as pd
+
+from deltalake import DeltaTable
+from deltalake.writer import write_deltalake
+
+# Load Delta Lake table
+print("*" * 80)
+dt = DeltaTable("../../data/diabetes-deltalake/diabetes_1")
+print("Current Delta table:")
+print(dt.to_pandas())
+df = pd.DataFrame()
+j = 0
+features = [
+    "Pregnancies",
+    "Glucose",
+    "BloodPressure",
+    "SkinThickness",
+    "Insulin",
+    "BMI",
+    "DiabetesPedigreeFunction",
+    "Age",
+]
+while j < 1000:
+    new_row = {}
+    for feature in features:
+        if feature == "Pregnancies":
+            new_row[feature] = random.randint(0, 17)
+        elif feature == "Glucose":
+            new_row[feature] = random.randint(0, 199)
+        elif feature == "BloodPressure":
+            new_row[feature] = random.randint(0, 122)
+        elif feature == "SkinThickness":
+            new_row[feature] = random.randint(0, 99)
+        elif feature == "Insulin":
+            new_row[feature] = random.randint(0, 846)
+        elif feature == "DiabetesPedigreeFunction":
+            new_row[feature] = random.uniform(0.078000, 2.420000)
+        elif feature == "BMI":
+            new_row[feature] = random.uniform(0.0, 67.1)
+        else:
+            new_row[feature] = random.randint(21, 81)
+
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    j += 1
+# Append to create new versions
+# Take a look at this for more details: https://delta.io/blog/2022-10-15-version-pandas-dataset/
+write_deltalake(dt, df, mode="append")
+print("Final Delta table:")
+dt2 = DeltaTable("../data/diabetes-deltalake/diabetes_1")
+print(dt2.to_pandas())
